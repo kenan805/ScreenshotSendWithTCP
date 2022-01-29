d8140cd32238284aae3148bc953c06bc0e5ff521
Kənan İdayatov <idayatovkenan7@gmail.com>
2022-01-29 22:29:00 +0400

    First commit.

---
+++ TCP_Client/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TCP_Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+
+        }
+
+        public byte[] ReadAllBytesAsync(BinaryReader reader)
+        {
+            const int bufferSize = 1024;
+            using (var ms = new MemoryStream())
+            {
+                byte[] buffer = new byte[bufferSize];
+                int count;
+                while ((count = reader.Read(buffer, 0, buffer.Length)) != 0)
+                    ms.Write(buffer, 0, count);
+                return ms.ToArray();
+            }
+
+        }
+        public static BitmapImage LoadImage(byte[] imageData)
+        {
+            var image = new BitmapImage();
+            using (var mem = new MemoryStream(imageData))
+            {
+                mem.Position = 0;
+                image.BeginInit();
+                image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.StreamSource = mem;
+                image.EndInit();
+
+            }
+            image.Freeze();
+            return image;
+        }
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var client = new TcpClient();
+                client.Connect("127.0.0.1", 45678);
+                using (var stream = client.GetStream())
+                {
+                    var bw = new BinaryWriter(stream);
+                    var br = new BinaryReader(stream);
+
+                    while (true)
+                    {
+                        var imageData = await Task.Run(() => ReadAllBytesAsync(br));
+
+                        imgFromServer.Source = LoadImage(imageData);
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Client error: " + ex.Message);
+            }
+        }
+
+    }
+}
--- TCP_Server/Program.cs
+++ TCP_Server/Program.cs
@@ -1,2 +1,59 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Net;
+using System.Net.Sockets;
+
+MainAsync(args).GetAwaiter().GetResult();
+
+async Task MainAsync(string[] args)
+{
+    try
+    {
+        var ipAddress = IPAddress.Parse("127.0.0.1");
+        var listener = new TcpListener(ipAddress, 45678);
+
+        listener.Start(100);
+
+        while (true)
+        {
+            Console.WriteLine("Listener...");
+            var client = listener.AcceptTcpClient();
+            Console.WriteLine("New client connected...(Write \"send\")");
+
+            using (var stream = client.GetStream())
+            {
+                var bw = new BinaryWriter(stream);
+                var br = new BinaryReader(stream);
+
+                if (Console.ReadLine()?.ToLower() == "send")
+                    bw.Write(await Task.Run(() => ScreenConsole()));
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
+
+
+
+static byte[] ScreenConsole()
+{
+
+    Bitmap memoryImage = new Bitmap(1920, 1080);
+    Size s = new Size(memoryImage.Width, memoryImage.Height);
+    Graphics memoryGraphics = Graphics.FromImage(memoryImage);
+    memoryGraphics.CopyFromScreen(0, 0, 0, 0, s);
+
+
+    using (var ms = new MemoryStream())
+    {
+        memoryImage.Save(ms, ImageFormat.Png);
+        Console.WriteLine("Screen oldu");
+        return ms.ToArray();
+    }
+
+}
